4bc2fdfc4c7e951a4083a5bfc6b78392bc53ac9e
Tanawat Wirattangsakul <webmaster.tanawat@gmail.com>
2020-04-11 21:06:51 +0700

    done fetchedOrder

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -22,7 +22,7 @@ class ContactData extends Component {
           this.setState({ loading: true });
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.totalPrice,
+      price: this.props.price,
       customer: {
         name: "Tanawat",
         address: {
