4644d3eef7ba97943f6d141066aba4154965d29c
Tanawat Wirattangsakul <webmaster.tanawat@gmail.com>
2020-04-22 12:35:48 +0700

    refact switch case

--- src/store/reducers/cakeBuilder.js
+++ src/store/reducers/cakeBuilder.js
@@ -13,41 +13,53 @@ const initialState = {
     lemon: 199,
     orange: 199
   };
-const reducer = (state = initialState, action) => {
-    switch(action.type) {
-        case actionTypes.ADD_INGREDIENT:
-            const updatedIngredient =  { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 }
+
+const addIngredient = (state, action) => {
+    const updatedIngredient =  { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 }
             const updatedIngredients = updateObject(state.ingredients, updatedIngredient)
             const updatedState = {
                 ingredients: updatedIngredients,
                 totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
             }
             return updateObject(state, updatedState)
-        case actionTypes.REMOVE_INGREDIENT:
-            const removeUpdatedIngredient =  { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 }
+}
+
+const removeIngredient = (state, action) => {
+    const removeUpdatedIngredient =  { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 }
             const removeUpdatedIngredients = updateObject(state.ingredients, removeUpdatedIngredient)
             const removeUpdatedState = {
                 ingredients: removeUpdatedIngredients,
                 totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
             }
             return updateObject(state, removeUpdatedState)
-        case actionTypes.SET_INGREDIENTS:
-            return updateObject(state, {
-                ingredients: {
-                    chocolate: action.ingredients.chocolate,
-                    strawberry: action.ingredients.strawberry,
-                    blueberry: action.ingredients.blueberry,
-                    vanilla: action.ingredients.vanilla,
-                    lemon: action.ingredients.lemon,
-                    orange: action.ingredients.orange
-                },
-                totalPrice: 0,
-                error: false
-            })
-            case actionTypes.FETCH_INGREDIENTS_FAILED: 
-            return updateObject(state, {error: true})
-        default:
-            return state
+}
+
+const setIngredient = (state, action) => {
+    return updateObject(state, {
+        ingredients: {
+            chocolate: action.ingredients.chocolate,
+            strawberry: action.ingredients.strawberry,
+            blueberry: action.ingredients.blueberry,
+            vanilla: action.ingredients.vanilla,
+            lemon: action.ingredients.lemon,
+            orange: action.ingredients.orange
+        },
+        totalPrice: 0,
+        error: false
+    })
+}
+
+const fetchIngredient = (state, action) => {
+    return updateObject(state, {error: true})
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case actionTypes.ADD_INGREDIENT: return addIngredient(state, action)
+        case actionTypes.REMOVE_INGREDIENT: return removeIngredient(state, action)
+        case actionTypes.SET_INGREDIENTS: return setIngredient(state, action)
+        case actionTypes.FETCH_INGREDIENTS_FAILED: return fetchIngredient(state, action)
+        default: return state
     }
 }
 
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -6,31 +6,50 @@ const initialState = {
     purchased: false
 }
 
+const purchaseInit = (state, action) => {
+    return updateObject(state, {purchased: false})
+}
+
+const purchaseCakeStart = (state, action) => {
+    return updateObject(state, {loading:true}) 
+ }
+
+const purchaseCakeSuccess = (state, action) => {
+    const newOrder = updateObject(action.orderData, {id:action.orderId})
+    return updateObject(state, {loading:false,
+        orders: state.orders.concat(newOrder),
+        purchased:true 
+    })
+}
+
+const purchaseCakeFail = (state, action) => {
+    return updateObject(state, {loading:false})
+}
+
+const fetchOrderStart = (state, action) => {
+    return updateObject(state,{loading:true})
+}
+
+const fetchOrderSuccess = (state, action) => {
+    return updateObject(state, { orders: action.orders,
+        loading: false
+        })
+}
+
+const fetchOrderFail = (state, action) => {
+    return updateObject(state, {loading:false})
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.PURCHASE_INIT:
-            return updateObject(state, {purchased:false})
-        
-        case actionTypes.PURCHASE_CAKE_START:
-            return updateObject(state, {loading:true}) 
-        case actionTypes.PURCHASE_CAKE_SUCCESS:
-            const newOrder = updateObject(action.orderData, {id:action.orderId})
-            return updateObject(state, {loading:false,
-                orders: state.orders.concat(newOrder),
-                purchased:true 
-            })
-        case actionTypes.PURCHASE_CAKE_FAIL:
-            return updateObject(state, {loading:false})
-        case actionTypes.FETCH_ORDERS_START: 
-            return updateObject(state,{loading:true})
-        case actionTypes.FETCH_ORDERS_SUCCESS:
-            return updateObject(state, { orders: action.orders,
-                loading: false
-                })
-        case actionTypes.FETCH_ORDERS_FAIL:
-            return updateObject(state, {loading:false})
-        default: 
-            return state
+        case actionTypes.PURCHASE_INIT: return purchaseInit(state, action)
+        case actionTypes.PURCHASE_CAKE_START: return purchaseCakeStart(state, action)
+        case actionTypes.PURCHASE_CAKE_SUCCESS: return purchaseCakeSuccess(state, action)
+        case actionTypes.PURCHASE_CAKE_FAIL: return purchaseCakeFail(state, action)
+        case actionTypes.FETCH_ORDERS_START: return fetchOrderStart(state, action)
+        case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrderSuccess(state, action)
+        case actionTypes.FETCH_ORDERS_FAIL: return fetchOrderFail(state, action)
+        default: return state
     }
 }
 
